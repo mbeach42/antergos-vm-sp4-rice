571370ff7c76df01fd2b7d0b8a292101e25e9c5b
Unknown <mbeach@phas.ubc.ca>
2018-01-15 13:54:07 -0500

    Added install script and terminfo

---
+++ install.sh
@@ -0,0 +1,52 @@
+##################################################
+# Installation script for Arch linux/Antergos setup
+##################################################
+
+# Replace yaourt with trizen
+yaourt -S trizen-git
+
+# Essential terminal related things
+trizen -S --noconfirm --noedit zsh git antigen-git termite ranger base16-git
+
+# i3-related desktop things
+trizen -S --noconfirm --noedit i3-gaps-next-git i3-blocks-git i3status-git
+rofi-git
+dmemu polybar-git compton compton-conf-git ttf-material-icons ttf-font-awesome-4
+
+# File manger related 
+trizen -S --noconfirm --noedit dropbox pantheon-files pantheon-files-plugin-dropbox-bzr 
+
+# Fonts
+trizen -S --noconfirm --noedit tf-fira-code-git ttf-hack otf-fira-sans otf-fira-mono otf-san-francisco ttf-ms-fonts ttf-google-fonts-git nerd-fonts-complete
+
+# Text editor 
+trizen -S --noconfirm --noedit vim vim-julia-git
+cp -r ~/julia-vim/* ~/.vim
+sudo pacman -S xmodmap
+
+# Communication things
+trizen -S --noconfirm --noedit slack-desktop qutebrowser thunderbird 
+
+# Other trivial things
+trizen -S --noconfirm --noedit neofetch feh
+
+# Programming
+sudo pacman -S julia
+CONTREPO=https://repo.continuum.io/archive/
+ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+wget -O ~/anaconda.sh $CONTREPO$ANACONDAURL
+bash ~/anaconda.sh
+rm ~/anaconda.sh
+cd
+
+# Remove some unnessesary things from installing openbox
+trizen -R antergos-openbox-meta kalu
+
+# Update
+trizen -Syu
+sudo pacman -Syy
+sudo pacman -Suu
+
+# Print number of packages
+trizen -Qm
+trizen -Qqm | wc -l
---
+++ julia-install.jl
@@ -0,0 +1,11 @@
+# Julia install favourite packages script 
+# By Matthew J. S. Beach
+
+Pkg.add("PyPlot")
+Pkg.add("NPZ")
+Pkg.add("HDF5")
+Pkg.add("IJulia")
+Pkg.add("DataFrames")
+Pkg.add("Stats")
+Pkg.add("Distributions")
+Pkg.add("TensorFlow")
